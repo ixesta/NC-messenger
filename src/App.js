126af2a965f1d075bde0e40462b77c8bf0e23a61
Jonny Rathbone <jonnyrathbone@gmail.com>
2018-04-18 15:14:42 +0100

    add and load messages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
 import {db} from './config';
+import moment from 'moment';
 
 class App extends Component {
-
-    componentDidMount() {
-        db.collection("users").add({
-            first: "Ada",
-            last: "Lovelace",
-            born: 1815
-        })
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
-        })
-        .catch(function(error) {
-            console.error("Error adding document: ", error);
-        });        
+    state = {
+        messages : [],
+        newMessageText : '',
+        user : 'Jonny'
     }
-  render() {
-    return (
-      <div className="App">
-        Home
-      </div>
-    );
-  }
+    componentDidMount () {
+        db.collection('messages').onSnapshot(snap => {
+            const {messages} = this.state;
+            const newMessages = snap.docChanges.reduce((messages, change) => {
+                console.log(messages);
+                if (change.type === 'added') {
+                    messages.push(change.doc.data())
+                }
+                return messages;
+            }, []);
+            this.setState({
+                messages : [...messages, ...newMessages]
+            })
+        })
+    }
+
+    render() {
+        const {messages, newMessageText} = this.state;
+        return (
+            <div className="App">
+                <ul>
+                    {messages.map((message, i) => 
+                        <li key={`${i}${message}`}>{message.user}: {message.text}</li>
+                    )}
+                </ul>
+                <input 
+                    type="text"
+                    value={newMessageText}
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleKeyDown}
+                />
+            </div>
+        );
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            newMessageText : e.target.value
+        })
+    }
+
+    handleKeyDown = (e) => {
+        if (e.which === 13) this.sendMessage();
+    }
+
+    sendMessage = () => {
+        const {newMessageText, user} = this.state;
+        const submission = {
+            text : newMessageText,
+            user,
+            timestamp : moment().format()
+        }
+        db.collection('messages').add(submission);
+        this.setState({
+            newMessageText : ''
+        })
+    }
 }
 
 export default App;
